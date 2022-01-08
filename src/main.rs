4f3ec7a9ddd6366f23d2cbc08a4e0c819c462d56
Levi Cook <levicook@gmail.com>
2022-01-07 17:29:26 -0700

    mine token holders (owners)

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use borsh::de::BorshDeserialize;
 use gumdrop::Options;
 use metaplex_token_metadata::state::Metadata;
 use rusqlite::{params, Connection};
+use serde::Deserialize;
+use serde_json::json;
 use solana_account_decoder::UiAccountEncoding;
 use solana_client::{
     rpc_client::RpcClient,
     rpc_config::{RpcAccountInfoConfig, RpcProgramAccountsConfig},
     rpc_filter::{Memcmp, MemcmpEncodedBytes, RpcFilterType},
+    rpc_request::RpcRequest,
+    rpc_response::Response,
 };
-use solana_sdk::account::ReadableAccount;
+use solana_sdk::{account::ReadableAccount, program_pack::Pack, pubkey::Pubkey};
+use spl_token::state::Account;
 use std::{error::Error, time::Duration};
 
 #[derive(Clone, Debug, Options)]
@@ -33,7 +38,7 @@ fn default_rpc_url() -> String {
 enum Command {
     MineHolders(MineHolders),
     MineMetadata(MineMetadata),
-    MineTransactions(MineTransactions),
+    // MineTransactions(MineTransactions),
 }
 
 #[derive(Clone, Debug, Options)]
@@ -48,11 +53,11 @@ struct MineHolders {
     creator_address: String,
 }
 
-#[derive(Clone, Debug, Options)]
-struct MineTransactions {
-    #[options(help = "account id")]
-    account_id: String,
-}
+// #[derive(Clone, Debug, Options)]
+// struct MineTransactions {
+//     #[options(help = "account id")]
+//     account_id: String,
+// }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -62,17 +67,78 @@ async fn main() -> Result<(), Box<dyn Error>> {
         Some(command) => match command {
             Command::MineHolders(opts) => mine_holders(args, opts).await,
             Command::MineMetadata(opts) => mine_metadata(args, opts).await,
-            Command::MineTransactions(opts) => mine_transactions(args, opts).await,
+            // Command::MineTransactions(opts) => mine_transactions(args, opts).await,
         },
     }
 }
 
-async fn mine_holders(_args: Args, _opts: MineHolders) -> Result<(), Box<dyn Error>> {
+async fn mine_holders(args: Args, _opts: MineHolders) -> Result<(), Box<dyn Error>> {
+    let timeout = Duration::from_secs(500); // TODO read from Args?
+    let rpc = RpcClient::new_with_timeout(args.rpc, timeout);
+
+    let db = Connection::open(args.db)?;
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS holders (
+            mint_address   text primary key,
+            holder_address text
+        )",
+        params![],
+    )?;
+
+    let mut stmt = db.prepare("SELECT mint_address FROM metadata")?;
+    let mut rows = stmt.query([])?;
+    while let Some(row) = rows.next()? {
+        let mint_address: String = row.get(0)?;
+        let mint_address = mint_address.parse()?;
+
+        let token_accounts = get_token_largest_accounts(&rpc, mint_address)?;
+        let token_accounts = token_accounts.value;
+        for token_account in token_accounts {
+            if token_account.amount == "1" {
+                let account = rpc.get_account(&token_account.address.parse()?)?;
+                let account = Account::unpack(&mut account.data())?;
+                db.execute(
+                    "DELETE FROM holders WHERE mint_address = ?1",
+                    params![mint_address.to_string()],
+                )?;
+                db.execute(
+                    "INSERT INTO holders (mint_address, holder_address) VALUES (?1, ?2)",
+                    params![mint_address.to_string(), account.owner.to_string()],
+                )?;
+            }
+        }
+    }
+
     Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RpcTokenAccounts {
+    pub address: String,
+    pub amount: String,
+    pub decimals: u8,
+}
+
+fn get_token_largest_accounts(
+    rpc: &RpcClient,
+    mint_address: Pubkey,
+) -> Result<Response<Vec<RpcTokenAccounts>>, Box<dyn Error>> {
+    let method = "getTokenLargestAccounts";
+    let request = RpcRequest::Custom { method };
+    let params = json!([mint_address.to_string()]);
+    Ok(rpc.send(request, params)?)
 }
 
 async fn mine_metadata(args: Args, opts: MineMetadata) -> Result<(), Box<dyn Error>> {
     let db = Connection::open(args.db)?;
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS creators (
+            creator_address  text,
+            metadata_address text,
+            UNIQUE(creator_address, metadata_address)
+        )",
+        params![],
+    )?;
     db.execute(
         "CREATE TABLE IF NOT EXISTS metadata (
             metadata_address text primary key,
@@ -81,7 +147,7 @@ async fn mine_metadata(args: Args, opts: MineMetadata) -> Result<(), Box<dyn Err
         params![],
     )?;
 
-    let timeout = Duration::from_secs(500);
+    let timeout = Duration::from_secs(500); // TODO read from Args?
     let rpc = RpcClient::new_with_timeout(args.rpc, timeout);
 
     let metadata_accounts = rpc.get_program_accounts_with_config(
@@ -112,41 +178,25 @@ async fn mine_metadata(args: Args, opts: MineMetadata) -> Result<(), Box<dyn Err
     )?;
 
     for (metadata_address, metadata) in metadata_accounts {
-        if {
-            let count: Result<u8, rusqlite::Error> = db.query_row(
-                "SELECT COUNT(*) FROM metadata WHERE metadata_address like ?1",
-                params![opts.creator_address.to_string()],
-                |row| row.get(0),
-            );
-            count? > 0
-        } {
-            continue;
-        }
-
         let metadata = Metadata::deserialize(&mut metadata.data())?;
         db.execute(
-            "INSERT INTO metadata (
-                metadata_address,
-                mint_address
-            ) VALUES (
-                ?1,
-                ?2
-            )",
+            "INSERT OR REPLACE INTO metadata (metadata_address, mint_address) VALUES (?1, ?2)",
             params![metadata_address.to_string(), metadata.mint.to_string()],
+        )?;
+        db.execute(
+            "INSERT OR REPLACE INTO creators (creator_address, metadata_address) VALUES (?1, ?2)",
+            params![opts.creator_address, metadata_address.to_string()],
         )?;
     }
 
     Ok(())
 }
 
-async fn mine_transactions(args: Args, opts: MineTransactions) -> Result<(), Box<dyn Error>> {
-    let rpc = RpcClient::new(args.rpc);
-
-    let account_id = opts.account_id.parse()?;
-    let account = rpc.get_account(&account_id)?;
-    let _deleteme = account;
-
-    let _db = Connection::open(args.db)?;
-
-    Ok(())
-}
+// async fn mine_transactions(args: Args, opts: MineTransactions) -> Result<(), Box<dyn Error>> {
+//     let rpc = RpcClient::new(args.rpc);
+//     let account_id = opts.account_id.parse()?;
+//     let account = rpc.get_account(&account_id)?;
+//     let _deleteme = account;
+//     let _db = Connection::open(args.db)?;
+//     Ok(())
+// }
